fc18a175603233a1ce341e8c24fa5efd847948ff
Thiago Henrique da Silva Cruz <thiago.cruz@db1.com.br>
2023-08-18 07:57:47 -0300

    test: ensur the create use case return right return in sucess and error case

--- src/test/src/data/TaskSchedulerRepository.ts
+++ src/test/src/data/TaskSchedulerRepository.ts
@@ -1,7 +1,8 @@
 import { type TaskScheduleInterface } from '../../../domain/protocols/TaskScheduleInterface'
+import * as scheduler from '../../mock/schedule'
 
 export const repository: jest.Mocked<TaskScheduleInterface> = {
-  saveSchedule: jest.fn(),
+  saveSchedule: jest.fn().mockResolvedValue(scheduler.mock.initialMockPost),
   getAllSchedule: jest.fn(),
   getScheduleByName: jest.fn(),
   updateSchedule: jest.fn(),
--- src/test/src/domain/features/CreateTaskSchedulerUseCase.spec.ts
+++ src/test/src/domain/features/CreateTaskSchedulerUseCase.spec.ts
@@ -1,29 +1,23 @@
 import { HttpResponse } from '../../../../presentation/helpers/HttpResponse'
 import { MissingParamError } from '../../../../presentation/helpers/MissingParamError'
 import * as scheduler from '../../../mock/schedule'
-import { repository } from '../../../src/data/TaskSchedulerRepository'
 import { CreateTaskSchedulerUseCase } from '../../../../domain/features/CreateTaskSchedulerUseCase'
-
-jest.mock('../../../../domain/features/CreateTaskSchedulerUseCase')
-
-const CreateUseCaseMock = CreateTaskSchedulerUseCase as jest.Mock<CreateTaskSchedulerUseCase>
+import { repository } from '../../data/TaskSchedulerRepository'
 
 describe('CREATE USE CASE', () => {
   it('should return missing param error if no payload is provided', async () => {
-    /* const payload = scheduler.mock.mockRequestWithoutName
-    const sut = new CreateTaskSchedulerUseCase(payload, repository)
-    sut.execute()
-    expect(sut).toEqual(new MissingParamError('name')) */
+    const payload = scheduler.mock.mockRequestWithoutName
+
+    const sut = await new CreateTaskSchedulerUseCase(payload, repository)
+
+    expect(await sut.execute()).toEqual(new MissingParamError('name'))
   })
 
   it('should return a new scheduler if right payload is provided', async () => {
-    /* const payload = scheduler.mock.mockRequest
-    CreateUseCaseMock.mockImplementation((): any => ({
-      execute: jest.fn(async () => await Promise.resolve(HttpResponse.badRequest('name or description')))
-    }))
-    const sut = new CreateTaskSchedulerUseCase(payload, repository)
-    console.log(sut)
+    const payload = scheduler.mock.mockRequest
 
-    expect(sut.execute()).toEqual('') */
+    const sut = await new CreateTaskSchedulerUseCase(payload, repository)
+
+    expect(await sut.execute()).toEqual(HttpResponse.goodRequest(scheduler.mock.initialMockPost))
   })
 })
